fb08c3e886a10a42a41e72ecca8768488bd93a9a
nodensera <nodensera@gmail.com>
2021-10-08 19:09:16 -0500

    js - styles updated !demo section

---
+++ script.js
@@ -0,0 +1,23 @@
+const song_list = {
+    "01": {
+        title: "Volta comigo bb",
+        artist: "Frank",
+        genre: "Sertanejo",
+        duration: "3:30", 
+        track: "volta-comigo-bb.mp3",
+    },
+    "02": {
+        title: "Se for amor",
+        artist: "Frank",
+        genre: "Bosa nova",
+        duration: "4:02", 
+        track: "se-for-amor.mp3",
+    },
+    "03": {
+        title: "Tem que ser agora",
+        artist: "Frank",
+        genre: "Frevo",
+        duration: "3:30", 
+        track: "tem-que-ser-agora.mp3",
+    }
+}
